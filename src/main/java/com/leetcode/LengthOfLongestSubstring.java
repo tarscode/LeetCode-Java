13240095eaf47462af047bf9bea3ac9993526db3
刘洋 <liuyang3@corp.netease.com>
2020-06-21 19:32:00 +0800

    feat:MinSubArrayLen

--- src/main/java/com/leetcode/LengthOfLongestSubstring.java
+++ src/main/java/com/leetcode/LengthOfLongestSubstring.java
@@ -9,7 +9,7 @@ package com.leetcode;
 /**
  *给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
  *
- * @author 刘洋 (liuyang3@corp.netease.com)
+ * @author 刘洋
  */
 public class LengthOfLongestSubstring {
     public static void main(String[] args) {
